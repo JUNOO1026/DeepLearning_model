c5503d6a12d4aa6e7a412c13a33ed47a6b96a80c
junwoo Lee <opskira1026@gmail.com>
2024-02-05 11:15:01 +0900

    Inception Net V1 Compelete

--- Inception_Net_v1.py
+++ Inception_Net_v1.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torchinfo import summary
 
 
 class BasicConvBlock(nn.Module):
@@ -25,10 +26,10 @@ class InceptionNet(nn.Module):
         self.branch2 = nn.Sequential(BasicConvBlock(in_channels, ch3x3re, kernel_size=1, stride=1, padding=1),
                                      BasicConvBlock(ch3x3re, ch3x3, kernel_size=3, stride=1, padding=1))
 
-        self.branch3 = nn.Sequential(BasicConvBlock(in_channels, ch5x5re, kernel_size=1, stirde=1, padding=1),
+        self.branch3 = nn.Sequential(BasicConvBlock(in_channels, ch5x5re, kernel_size=1, stride=1, padding=1),
                                      BasicConvBlock(ch5x5re, ch5x5, kernel_size=5, stride=1, padding=1))
 
-        self.brach4 = nn.Sequential(nn.MaxPool2d(3, stride=1, padding=1),
+        self.branch4 = nn.Sequential(nn.MaxPool2d(3, stride=1, padding=1),
                                     BasicConvBlock(in_channels, poolproj, kernel_size=1, stride=1, padding=1))
 
     def forward(self, x):
@@ -70,5 +71,72 @@ class InceptionNetV1(nn.Module):
         self.use_aux = use_aux
 
         self.conv1 = BasicConvBlock(3, 64, kernel_size=7, stride=2, padding=3)
-        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
-        self.conv2 =
+        self.maxpool1 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.conv2 = BasicConvBlock(64, 64, kernel_size=1, stride=2)
+        self.conv3 = BasicConvBlock(64, 192, kernel_size=3, stride=1, padding=1)
+        self.maxpool2 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.inception3a = InceptionNet(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = InceptionNet(256, 128, 128, 192, 32, 96, 64)
+        self.maxpool3 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.inception4a = InceptionNet(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = InceptionNet(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = InceptionNet(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = InceptionNet(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = InceptionNet(528, 256, 160, 320, 32, 128, 128)
+        self.maxpool4 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.inception5a = InceptionNet(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = InceptionNet(832, 384, 192, 382, 48, 128, 128)
+        if use_aux:
+            self.inceptionAux1 = InceptionAux(512, num_classes)
+            self.inceptionAux2 = InceptionAux(528, num_classes)
+        else:
+            self.inceptionAux1 = None
+            self.inceptionAux2 = None
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(p=drop_p)
+        self.linear = nn.Linear(1024, num_classes*1*1)
+
+        if init_weights:
+            for m in self.modules:
+                if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                    nn.init.trunc_normal_(m.weight, mean=0, std=0.01, a=-2, b=2)
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.maxpool2(x)
+        x = self.inception3a(x)
+        x = self.inception3b(x)
+        x = self.maxpool3(x)
+        x = self.inception4a(x)
+        if self.inceptionAux1 is not None and self.training:
+            inceptionAux1 = self.inceptionAux1(x)
+        else:
+            inceptionAux1 = None
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        if self.inceptionAux2 is not None and self.training:
+            inceptionAux2 = self.inceptionAux2(x)
+        else:
+            inceptionAux2 = None
+        x = self.inception4e(x)
+        x = self.maxpool4(x)
+        x = self.inception5a(x)
+        x = self.inception5b(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, dims=1)
+        x = self.dropout(x)
+        x = self.linear(x)
+        return x, inceptionAux1, inceptionAux2
+
+
+model = InceptionNetV1()
+summary(model, input_size=(2, 3, 224, 224), device="cuda")
+
+
--- VGGNet.py
+++ VGGNet.py
@@ -37,6 +37,25 @@ class VGGNet(nn.Module):
                     nn.init.normal_(m.weight, 0, 0.01)
                     nn.init.constant_(m.bias, 0)
 
+        # 위의 방법 즉, VGGNet에서 kaiming_normal_ 방식으로 weight를 initiallize한 이유는
+        # kaiming_normal 방식의 경우 ReLU activation과 같이 사용하며 그래디언트 완화 목적임.
+
+        # 여기서 trunc 가중치 초기화 방법을 굳이 안쓰는 이유는 다양한 크기의 커널을 병렬로 사용하는 InceptionNet과 다르며
+        # 네트워크의 성능에 대한 안정성을 유지하기 위해 각 가중치를 특정 구간으로 제한하는 trunc_normal 방식은 별로임.
+
+        # 예를들어, 동일한 커널로 학습되는 VGGNet의 경우 특정 커널 사이즈로 인해 네트워크 성능이 떨어지진 않음.
+
+        # 하지만, InceptionNet 처럼 여러 커널 사이즈로 학습이 되는 경우에는, 특정 커널 사이즈의 영향도가 너무 커버리면
+        # 다른 커널 사이즈로 학습된 부분이 무시되는 경향이 있을 수 있으므로 어느정도 특정 구간을 제한하여 값을 샘플링하는 것임.
+
+        # if init_weights:
+        #     for m in self.modules():
+        #         if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        #             nn.init.trunc_normal_(m.weight, mean=0.0, std=0.01, a=-2, b=2)
+        #         elif isinstance(m, nn.BatchNorm2d):
+        #             nn.init.constant_(m.weight, 1)
+        #             nn.init.constant_(m.bias, 0)
+
         # 예를들어 분산이 1인 random variable이 들어간다고 하면 각 weight가 곱해지고 더해지면 출력 노드의 variance(변화)는 커지게 됨. 이러면 학습에 도움을 주지 않음.
         # 이러한 문제를 막기 위해 Back Propagation에서의 분산을 1/N_out 으로 나눠주고 그 nonlinearity는 relu로 학습할 수 있도록 한다.
 
